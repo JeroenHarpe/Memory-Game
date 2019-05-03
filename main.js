0e9bc5dc84382b813c486cb6482312ccfe0139e1
Jeroen Harpe <43515079+JeroenHarpe@users.noreply.github.com>
2019-05-04 07:12:25 +0800

    All functions work:)

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ var colors = [
     'yellow',
     'purple',
     'orange',
-    'blue'
+    'blue',
+    'pink',
+    'cyan'
 ];
+//double the array
 var doubleColors = colors.reduce(function (res, current, index, array) {
     return res.concat([current, current]);
 }, []);
+// get a random color
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     // While there remain elements to shuffle...
@@ -25,6 +29,7 @@ function shuffle(array) {
 }
 var randomColors = shuffle(doubleColors);
 var counter = 1;
+// select div's and add the colors
 for (var i = 0; i < randomColors.length; i++) {
     if (counter <= randomColors.length) {
         var elem = document.querySelector(".nr" + counter);
@@ -34,35 +39,55 @@ for (var i = 0; i < randomColors.length; i++) {
 }
 var tiles = document.querySelectorAll('.tile');
 var flipCounter = 0;
+var colorsFlipped = '';
+tiles.forEach(function (tile) {
+    tile.addEventListener('click', function () {
+        tile.classList.add('flip');
+        // add to flipcounter when user clicks tile
+        if (tile.classList.contains('flip')) {
+            flipCounter++;
+        }
+        //check if there's a match
+        if (tile.classList.contains('flip')) {
+            var matchingTiles = document
+                .querySelectorAll('.tile.flip[style*="background: ' + tile.style.background + ';"]');
+            if (matchingTiles.length > 1) {
+                setTimeout(function () {
+                    gotPair();
+                    allchecked();
+                }, 1000);
+            }
+        }
+        // Clear tiles when there's not a pair
+        if (flipCounter >= 2) {
+            setTimeout(function () {
+                clearFlippedTiles();
+                flipCounter = 0;
+            }, 1000);
+        }
+    });
+});
+//clears the flipped tiles
 var clearFlippedTiles = function () {
     document.querySelectorAll('.tile.flip').forEach(function (tile) {
         tile.classList.remove('flip');
     });
 };
-tiles.forEach(function (tile) {
-    tile.addEventListener('click', function () {
-        tile.classList.add('flip');
-        if (tile.classList.contains('flip')) {
-            flipCounter++;
-            console.log(flipCounter);
-        }
-        if (flipCounter > 2) {
-            clearFlippedTiles();
-            flipCounter = 0;
-        }
+// When user got a pair
+var gotPair = function () {
+    var flipped = document.querySelectorAll('.flip');
+    flipped.forEach(function (tile) {
+        tile.removeAttribute("style");
+        tile.classList.remove('flip');
+        tile.innerHTML = '<i class="fas fa-check"></i>';
+        tile.classList.add('check');
+        tile.classList.remove('tile');
     });
-});
-// interface Tile {
-//     number: number;
-//     color: string;
-//     div: string;
-// }
-// class CreateTile implements Tile {
-//     number: number;
-//     color: string;
-//     div: string;
-// }
-// let x = document.querySelectorAll('.tile');
-//         x.addEventListener('click', function(){
-//             x.classList.toggle('flip')});
+};
+var allchecked = function () {
+    var checked = document.querySelectorAll('.check');
+    if (checked.length >= doubleColors.length) {
+        console.log('congrats!');
+    }
+};
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -4,15 +4,18 @@ let colors: string[] = [
     'yellow',
     'purple',
     'orange',
-    'blue'
+    'blue',
+    'pink',
+    'cyan'
 ]
+//double the array
 let doubleColors = colors.reduce(function (res, current, index, array) {
     return res.concat([current, current]);
 }, []);
 
 
 
-
+// get a random color
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -37,6 +40,7 @@ let randomColors = shuffle(doubleColors);
 
 let counter: number = 1;
 
+// select div's and add the colors
 for (let i = 0; i < randomColors.length; i++) {
     if (counter <= randomColors.length) {
         let elem = < HTMLElement > document.querySelector(`.nr${counter}`);
@@ -48,26 +52,66 @@ for (let i = 0; i < randomColors.length; i++) {
 let tiles = document.querySelectorAll < HTMLElement > ('.tile');
 
 let flipCounter : number = 0;
+let colorsFlipped : string = '';
 
+
+tiles.forEach(function (tile) {
+    tile.addEventListener('click', function () {
+        tile.classList.add('flip');
+        // add to flipcounter when user clicks tile
+        if (tile.classList.contains('flip')) {
+            flipCounter++
+        }
+          //check if there's a match
+          if (tile.classList.contains('flip')){
+            let matchingTiles = document
+                .querySelectorAll < HTMLElement > ('.tile.flip[style*="background: ' + tile.style.background +  ';"]');
+            if (matchingTiles.length > 1){       
+             setTimeout(() => {
+                gotPair();
+                allchecked();
+             }, 1000); 
+            }
+        }
+        // Clear tiles when there's not a pair
+        if (flipCounter >= 2) {
+            setTimeout(() => {
+            clearFlippedTiles();
+            flipCounter = 0;
+            }, 1000);
+        
+          
+        }
+      
+    
+    });
+});
+
+//clears the flipped tiles
 let clearFlippedTiles = () => {
     document.querySelectorAll < HTMLElement > ('.tile.flip').forEach((tile) => {
         tile.classList.remove('flip');
     });
 }
 
-tiles.forEach(function (tile) {
-    tile.addEventListener('click', function () {
-        tile.classList.add('flip');
-        if (tile.classList.contains('flip')) {
-            flipCounter++
-            console.log(flipCounter)
-        }
-        if (flipCounter > 2) {
-            clearFlippedTiles();
-            flipCounter = 0;
-        }
-    });
+// When user got a pair
+let gotPair = () => {
+let flipped = document.querySelectorAll('.flip');
+flipped.forEach(tile => {
+tile.removeAttribute("style");
+tile.classList.remove('flip');  
+tile.innerHTML = '<i class="fas fa-check"></i>';
+tile.classList.add('check');
+tile.classList.remove('tile');  
 });
+}
+
+let allchecked = () => {
+let checked = document.querySelectorAll('.check');
+if (checked.length >= doubleColors.length) {
+    console.log('congrats!');
+}
+}
 
 
 
@@ -76,30 +120,3 @@ tiles.forEach(function (tile) {
 
 
 
-
-
-
-
-
-// interface Tile {
-//     number: number;
-//     color: string;
-//     div: string;
-// }
-
-// class CreateTile implements Tile {
-//     number: number;
-//     color: string;
-//     div: string;
-// }
-
-
-
-
-
-
-
-
-// let x = document.querySelectorAll('.tile');
-//         x.addEventListener('click', function(){
-//             x.classList.toggle('flip')});
